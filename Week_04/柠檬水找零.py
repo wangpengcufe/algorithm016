ded53dda2f07443e7c63f81f5131c000c1890db7
wangpengcufe <wangpengcufe@163.com>
2020-09-29 23:13:58 +0800

    柠檬水找零

---
+++ Week_04/柠檬水找零.py
@@ -0,0 +1,22 @@
+# Ì°ĞÄ·¨£¬Ë«Ö¸Õë
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        num5 = num10 = 0
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                num5 += 1
+            elif bills[i] == 10:
+                if num5 > 0:
+                    num5 -= 1
+                    num10 +=1
+                else:
+                    return False 
+            else:
+                if num5 >=1 and num10 >= 1:
+                    num5 -= 1
+                    num10 -=1
+                elif num5 >=3:
+                    num5 = num5 - 3
+                else:
+                    return False
+        return True
